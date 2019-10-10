5d612987f465ee9691df78f763fe457472b7fa49
dmbcjr <dcurranjr@gmail.com>
2019-10-10 03:04:34 +0100

    Player Movement_1
    -added scripts for basic player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+   
+    //**to be possibly used instead of editing mass 
+    //ie force * jumpSpeed * deltaTime
+    //public float jumpSpeed = 10f;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        
+       
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+            Debug.Log("is jump");
+
+        }
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime);
+
+            print("Rotating left");
+
+        }else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.forward * Time.deltaTime);
+            print("Rotating right");
+        }
+
+        
+
+    }
+}
