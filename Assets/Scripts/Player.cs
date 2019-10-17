50b85e2a65d17e4b58ebeba8ef73cf58539fcb8d
dmbcjr <dcurranjr@gmail.com>
2019-10-17 18:56:52 +0100

    Audio Source
    -Added AudioSource component
    -Added code to turn the sound on/off
    -Added notes on how the engines should sound in future

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,13 +9,18 @@ public class Player : MonoBehaviour
    
     //**to be possibly used instead of editing mass 
     //ie force * jumpSpeed * deltaTime
-    //public float jumpSpeed = 10f;
+    public float jumpSpeed = 5f;
 
     Rigidbody rb;
+
+    AudioSource m_PlayerAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        m_PlayerAudioSource = GetComponent<AudioSource>();
+
     }
 
 
@@ -23,29 +28,48 @@ public class Player : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+       
     }
 
     private void PlayerMovement()
     {
         
        
-
+        /*
+         * Audio Notes
+         * Audio of engine should sound in sync with thrust values
+         * At the moment it just shuts off
+         * The engine should sound like its ramping up and down
+         * i.e lerp through values depending on whether the player is thrust
+         * 0 being off 1 being 100% thrust/pitch or volume
+         * */
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up);
             Debug.Log("is jump");
 
+            if(!m_PlayerAudioSource.isPlaying)
+            m_PlayerAudioSource.Play();
+           
+
+        }else 
+        {
+            
+            
+            m_PlayerAudioSource.Pause();
+            
         }
+
 
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.forward * Time.deltaTime);
+            transform.Rotate(Vector3.forward * jumpSpeed * Time.deltaTime);
 
             print("Rotating left");
 
         }else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(-Vector3.forward * Time.deltaTime);
+            transform.Rotate(-Vector3.forward * jumpSpeed * Time.deltaTime);
             print("Rotating right");
         }
 
